2216caa575587d65771aa5424f11509effe8c292
BarcoBasureroB <168384492+BarcoBasureroB@users.noreply.github.com>
2024-06-09 16:44:18 +0000

    a

--- AVL/main.cpp
+++ AVL/main.cpp
@@ -4,3 +4,49 @@
 
 using namespace std;
 
+Nodo* busqueda(Nodo* &raiz, int dato)
+{
+    if(raiz == nullptr)
+    {
+        return nullptr;
+    }
+
+    if(raiz->dato == dato)
+    {
+        return raiz;
+    }
+
+    if(dato > raiz->dato)
+    {
+        return busqueda(raiz->hijoDer, dato);
+    }
+    return busqueda(raiz->hijoIzq, dato);
+}
+
+
+Nodo* insercion(Nodo* &raiz, int dato)
+{
+    if(raiz == nullptr)
+    {
+        return new Nodo(dato);
+    }   
+
+    Nodo* aux = busqueda(raiz, dato);
+
+    if(aux == nullptr)
+    {
+        if(raiz->dato > dato)
+        {
+            raiz->hijoIzq = insercion(raiz->hijoIzq, dato);
+        }
+        else 
+        {
+            raiz->hijoDer = insercion(raiz->hijoDer, dato);
+        }
+    }
+    else
+    {
+        cout<<"Número Repetido"<<endl;
+    }
+    return raiz;
+}
